42f7850423fa8486f735eb44b0c81468cc2be713
mahdi hamzeh <mahdi2001h@gmail.com>
2023-05-15 20:57:37 +0330

    fix wifi

--- package/rtl8188eus/rtl8188eus.mk
+++ package/rtl8188eus/rtl8188eus.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 RTL8188EUS_VERSION = v5.3.9
-RTL8188EUS_SITE = git://github.com/aircrack-ng/rtl8188eus.git
+RTL8188EUS_SITE = https://github.com/mahdi2001h/rtl8188eus/releases/download/v5.3.9-stable
+RTL8188EUS_SOURCE = rtl8188eus-5.3.9.tar.xz
 RTL8188EUS_LICENSE = GPL-2.0
 RTL8188EUS_MODULE_MAKE_OPTS = CONFIG_RTL8188EU=m
 
